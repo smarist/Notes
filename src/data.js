29dfb11c8c9020f85f7bc995c1159085c5a1c92d
smarist <stmarisnwokike@gmail.com>
2022-08-22 09:35:15 +0100

    creating note app

---
+++ src/data.js
@@ -0,0 +1,11 @@
+export const data = [
+    {
+        id: 1,
+        body: `f`,
+    },
+    { id: 2, body: `a` },
+    { id: 3, body: `b` },
+    { id: 4, body: `c` },
+    { id: 5, body: `d` },
+    { id: 6, body: `e` },
+]
